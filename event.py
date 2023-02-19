7cfe46da5de5476173c12116fcd6e0e54fbbb810
Reidmen Arostica <r.a.arostica.barrera@rug.nl>
2023-02-19 21:56:08 +0100

    added order and fill events

--- event.py
+++ event.py
@@ -36,3 +36,67 @@ class SignalEvent(Event):
         self.datetime = datetime
         self.signal_type = signal_type
         self.strength = strength
+
+
+class OrderEvent(Event):
+    """
+    Handles the event of sending an Order to an execution system.
+    The order contains a symbol (e.g. GOOG), a type (market or limit),
+    quantity and a direction.
+    """
+
+    def __init__(self, symbol: str, order_type: str, quantity: int, direction: str):
+        self.type = "ORDER"
+        self.symbol = symbol
+        self.order_type = order_type
+        self.quantity = quantity
+        self.direction = direction
+
+    def __repr__(self):
+        """String representation of the OrderEvent"""
+        return "Order: Symbol={}, Type={}, Quantity={}, Direction={}".format(
+            self.symbol, self.type, self.quantity, self.direction
+        )
+
+
+class FillEvent(Event):
+    """Encapsulated a filled order, as returned from a brokerage.
+    Stores the quantity of an instrument actually filled and at what price and
+    stores the commission of the trade from the brokerage.
+    """
+
+    def __init__(
+        self,
+        timeindex: str,
+        symbol: str,
+        exchange: str,
+        quantity: int,
+        direction: str,
+        fill_cost: float,
+        commission: float = None,
+    ):
+        self.type = "FILL"
+        self.timeindex = timeindex
+        self.symbol = symbol
+        self.exchange = exchange
+        self.direction = direction
+        self.fill_costs = fill_costs
+        if commission is None:
+            self.commission = self.compute_ib_commission()
+        else:
+            self.commission = commission
+
+    # TODO: check commission on interactive brokers and other providers
+    def compute_ib_commission(self):
+        """Computes the fees of trading based on an Interactive Brokers fee structure
+        for API, in USD.
+
+        It does not include exchange or ECN fees.
+        """
+        full_cost = 1.3
+        if self.quantity <= 500:
+            full_cost = max(1.3, 0.013 * self.quantity)
+        else:
+            full_cost = max(1.3, 0.008 * self.quantity)
+
+        return full_cost
