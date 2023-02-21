0242434e7fbf9048e690cd5cb9f8ca84c2b4f624
Reidmen Arostica <r.a.arostica.barrera@rug.nl>
2023-02-21 21:51:31 +0100

    added abstract for strategy

---
+++ strategy.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+import sys
+
+if sys.version < 3.9:
+    raise Exception(f"Upgrade Python, found {sys.version}")
+
+import datetime
+import queue
+
+import numpy as np
+import pandas as pd
+
+from event import SignalEvent
+
+
+class Strategy(ABC):
+    """Strategy is an abstract base class providing an interface for
+    all subsequent (inherited) strategy objects.
+
+    The goal of a strategy object is to generate a Signal object
+    for particular symbols based on the inputs of Bars (OHLCV) generated
+    by a DataHandler object.
+
+    Its designed for historic and live data since (inherited) strategies
+    are agnostic from the data source.
+    """
+
+    @abstractmethod
+    def compute_signals(self):
+        """Computes the mechanics for a list of singals."""
+        raise NotImplementedError("Must implement compute_signals()")
