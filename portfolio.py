59b0fbfc1cd5737f95697130bb24dd5478e71f97
Reidmen Arostica <r.a.arostica.barrera@rug.nl>
2023-02-24 19:08:56 +0100

    updated portfolio fill events

--- portfolio.py
+++ portfolio.py
@@ -71,4 +71,69 @@ class Portfolio:
         holdings["commission"] = 0.0
         holdings["total"] = self.initial_capital
 
-        return [holdings]
+        return holdings
+
+    def update_timeindex(self, event: Event):
+        """Adds a new record to the positions matrix for the current
+        market bar. This reflects the PREVIOUS bar, i.e. all current
+        market data at this stage is known (OHLCV).
+        """
+        latest_datetime = self.bars.get_latest_bar_datetime(self.symbol_list[0])
+        # update positions
+        positions = dict((k, v) for k, v in [(s, 0) for s in self.symbol_list])
+        positions["datetime"] = latest_datetime
+
+        for symbol in self.symbol_list:
+            positions[symbol] = self.current_positions[symbol]
+
+        self.all_positions.append(positions)
+
+        # update holdings
+        holdings = dict((k, v) for k, v in [(s, 0) for s in self.symbol_list])
+        holdings["datetime"] = latest_datetime
+        holdings["cash"] = self.current_holdings["cash"]
+        holdings["commission"] = self.current_holdings["commission"]
+        holdings["total"] = self.current_holdings["cash"]
+
+        for symbol in self.symbol_list:
+            market_value = self.current_positions[
+                symbol
+            ] * self.bars.get_latest_bar_value(s, "adj_close")
+            holdings[symbol] = market_value
+            holdings["total"] += market_value
+
+        self.all_holdings.append(holdings)
+
+    def update_positions_from_fill(self, fill: FillEvent):
+        """Takes a Fill object and updates the position martix to
+        reflect the new position."""
+        fill_direction = 0
+        if fill.direction == "BUY":
+            fill_direction = 1
+        if fill.direction == "SELL":
+            fill_direction = -1
+
+        self.current_positions[fill.symbol] += fill_direction + fill.quantity
+
+    def update_holdings_from_fill(self, fill: FillEvent):
+        """Takes a Fill object and updates the holdings matrix to
+        reflect the holdings value."""
+        fill_direction = 0
+        if fill.direction == "BUY":
+            fill_direction = 1
+        if fill.direction == "SELL":
+            fill_direction = -1
+
+        fill_cost = self.bars.get_latest_bar_value(fill.symbol, "adj_close")
+        cost = fill_direction * fill_cost * fill.quantity
+        self.current_holdings[fill.symbol] += cost
+        self.current_holdings["commission"] += fill.commission
+        self.current_holdings["cash"] -= (cost + fill.commission)
+        self.current_holdings["total"] -= (cost + fill.commission)
+
+    def update_using_fill_event(self, event: Event):
+        """Updates the portfolio current positions and holdings
+        using the fill event"""
+        if event.type == "FILL":
+            self.update_positions_from_fill(event)
+            self.update_holdings_from_fill(event)
