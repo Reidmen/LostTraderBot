510907b7e1d6033c4ad36c3e520b25cfbd56ba1f
Reidmen <r.rethmawn@gmail.com>
2023-05-21 14:10:31 +0200

    added remaining events

--- losttraderbot/event.cpp
+++ losttraderbot/event.cpp
@@ -1,8 +1,11 @@
 #include "event.hpp"
+#include <cstdio>
+#include <iostream>
+#include <ostream>
 #include <string>
 
 MarketEvent::MarketEvent() {
-    this -> type = 0;
+    this->type = 0;
 }
 
 SignalEvent::SignalEvent(
@@ -10,9 +13,53 @@ SignalEvent::SignalEvent(
         long long* timestamp,
         double signal, 
         std::string target) {
-    this -> type = 1;
-    this -> symbol = *symbol;
-    this -> timestamp = *timestamp;
-    this -> signal = signal;
-    this -> target = target;
+    this->type = 1;
+    this->symbol = *symbol;
+    this->timestamp = *timestamp;
+    this->signal = signal;
+    this->target = target;
+}
+
+OrderEvent::OrderEvent(
+        std::string* symbol,
+        std::string order_type,
+        double* quantity,
+        std::string* direction,
+        std::string target) {
+    this->type = 2;
+    this->symbol = *symbol;
+    this->order_type = order_type;
+    this->quantity = *quantity;
+    this->direction= *direction;
+    this->target = target;
+}
+
+void OrderEvent::logOrder() {
+    std::cout << "Order placed!" << std::endl;
+}
+
+FillEvent::FillEvent(
+        std::string* symbol,
+        long long* timestamp,
+        double* quantity,
+        std::string direction,
+        double cost,
+        std::string target) {
+    this->type = 3;
+    this->symbol = *symbol;
+    this->timestamp = *timestamp;
+    this->quantity = *quantity;
+    this->direction = direction;
+    this->cost = cost;
+    this->commission = computeCommission();
+    this->slippage = computeSlippage();
+    this->target = target;
+}
+
+double FillEvent::computeCommission() {
+    return 0.001 * cost;
+}
+
+double FillEvent::computeSlippage() {
+    return 0.0;
 }
--- losttraderbot/event.hpp
+++ losttraderbot/event.hpp
@@ -27,3 +27,41 @@ class SignalEvent: public Event {
                 std::string target);
 };
 
+class OrderEvent: public Event {
+    public:
+        std::string symbol;
+        std::string order_type;
+        double quantity;
+        std::string direction;
+
+        OrderEvent(
+                std::string* symbol,
+                std::string order_type,
+                double* quantity,
+                std::string* direction,
+                std::string target);
+
+        void logOrder();
+};
+
+class FillEvent: public Event {
+    public:
+        std::string symbol;
+        long long timestamp;
+        double quantity;
+        std::string direction;
+        double cost;
+        double commission;
+        double slippage;
+
+        FillEvent(
+                std::string* symbol,
+                long long* timestamp,
+                double* quantity,
+                std::string direction,
+                double cost,
+                std::string target);
+
+        double computeCommission();
+        double computeSlippage();
+};
