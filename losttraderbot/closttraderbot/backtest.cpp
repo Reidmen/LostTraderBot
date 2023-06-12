ea9e2e3cdc8714c4cc0eee9089b0db40045b6e8f
Reidmen <r.rethmawn@gmail.com>
2023-06-12 21:23:11 +0200

    updated data header and added backtest

---
+++ losttraderbot/closttraderbot/backtest.cpp
@@ -0,0 +1,65 @@
+#include "backtest.hpp"
+#include "event.hpp"
+#include "execution.hpp"
+#include "strategy.hpp"
+#include <iostream>
+#include <memory>
+#include <vector>
+
+Backtest::Backtest(
+        std::unique_ptr<std::vector<std::string>> symbols,
+        std::unique_ptr<std::string> csvDirectory,
+        std::unique_ptr<double> initialCapital
+        ) : exchange(&eventQueue, &dataHandler) {
+    this->symbols = *symbols;
+    this->csvDirectory = *csvDirectory;
+    this->initialCapital = *initialCapital;
+    this->continueBacktest = false;
+    this->dataHandler = HistoricCSVDataHandler(&dataHandler, symbols, initialCapital);
+}
+
+void Backtest::run(std::unique_ptr<TradingStrategy> strategy) {
+    continueBacktest = true;
+
+    dataHandler.loadData();
+    dataHandler.updateBars();
+
+    std::cout << "Starting backtesting..." << std::endl;
+
+    while (continueBacktest) {
+        while (!eventQueue.empty()) {
+            auto event = eventQueue.front();
+            eventQueue.pop();
+
+            switch (event->type) {
+                case 0:
+                    {
+                        strategy->calculateSignals();
+                        portfolio.update();
+                        break;
+                    }
+                case 1:
+                    {
+                        auto signal = std::dynamic_pointer_cast(event); 
+                        if (event->target == "ALGO") {
+                            portfolio.onSignal(signal);
+                        }
+                    }
+                case 2:
+                    {
+                        auto order = std::dynamic_pointer_cast(event); 
+                        exchange.executeOrder(order);
+                        // order->logOrder()
+                        break;
+                    }
+            
+            }
+        }
+
+        dataHandler.updateBars();
+    }
+
+    std::cout << "Backtest ended\n" << std::endl;
+    std::cout << "Performance metrics" << std::endl;
+    portfolio.getMetrics();
+}
--- losttraderbot/closttraderbot/data.hpp
+++ losttraderbot/closttraderbot/data.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <cstddef>
 #include <fstream>
 #include <string>
