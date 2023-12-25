dea48d9bc38b68de30eb90acb1b65a451c0ce296
Reidmen <r.rethmawn@gmail.com>
2023-12-25 19:33:24 +0100

    adding some init methdos

--- src/cpp/portfolio.cpp
+++ src/cpp/portfolio.cpp
@@ -19,3 +19,54 @@ BasicPortfolio::BasicPortfolio(
     this->allHoldings = constructAllHoldings();
     this->currentHoldings = constructCurrentHoldings();
 }
+
+auto BasicPortfolio::constructAllPositions() -> MapPositionsType {
+    PositionsType innerMap;
+    for (auto symbol : *symbols) {
+        innerMap.insert({symbol, 0.0});
+    }
+    auto firstTimestamp = dataHandler->bar->first;
+    MapPositionsType map;
+    map.insert({firstTimestamp, innerMap});
+    return map;
+}
+
+auto BasicPortfolio::constructCurrentPositions() -> PositionsType {
+    PositionsType map;
+    for (auto symbol : *symbols) {
+        map.insert({symbol, 0.0});
+    }
+    return map;
+}
+
+auto BasicPortfolio::constructAllHoldings() -> MapPositionsType {
+    PositionsType innerMap;
+    for (auto symbol : *symbols) {
+        innerMap.insert({symbol, 0.0});
+    }
+    innerMap.insert({"cash", *initialCapital});
+    innerMap.insert({"commission", 0.0});
+    innerMap.insert({"slippage", 0.0});
+    innerMap.insert({"total", *initialCapital});
+    innerMap.insert({"returns", 0.0});
+    innerMap.insert({"equity_curve", 0.0});
+
+    auto firstTimestamp = dataHandler->bar->first;
+    MapPositionsType map;
+    map.insert({firstTimestamp, innerMap});
+
+    return map;
+}
+
+auto BasicPortfolio::constructCurrentHoldings() -> PositionsType {
+    PositionsType map;
+    for (auto symbol : *symbols) {
+        map.insert({symbol, 0.0});
+    }
+    map.insert({"cash", *initialCapital});
+    map.insert({"commission", 0.0});
+    map.insert({"slippage", 0.0});
+    map.insert({"total", *initialCapital});
+
+    return map;
+}
